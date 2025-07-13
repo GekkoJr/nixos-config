c2efad38156aa18f5feffa7354b9fd79874f9972
GekkoJr <gekkojr@proton.me>
2025-07-14 00:38:30 +0200

    fixed stylix :)

--- home/hypr/hyprland.nix
+++ home/hypr/hyprland.nix
@@ -1,4 +1,4 @@
-{...}:
+{config, ...}:
 {
 	wayland.windowManager.hyprland = {
 		enable = true;
@@ -65,8 +65,8 @@
 				gaps_in = "5";
 				gaps_out = "10";
 				border_size = "2";
-				"col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-				"col.inactive_border" = "rgba(595959aa)";
+				#"col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+				#"col.inactive_border" = "rgba(595959aa)";
 
 				layout = "dwindle";
 			};
--- modules/core/stylix/stylix.nix
+++ modules/core/stylix/stylix.nix
@@ -26,6 +26,5 @@
         };
         };
 
-        autoEnable = false;
     };
 }
