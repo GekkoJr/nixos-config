97c6c5ab4e577d1ed6f56639f75b7386d742147d
GekkoJr <gekkojr@proton.me>
2025-07-14 00:50:08 +0200

    turns out eww plugin is not in pkgs

--- home/core-programs/vscode.nix
+++ home/core-programs/vscode.nix
@@ -9,7 +9,7 @@
 				pkgs.vscode-extensions.bbenoist.nix # nix support
 				pkgs.vscode-extensions.ms-python.python # python support
 				pkgs.vscode-extensions.leonardssh.vscord # Discord rich prescence
-				pkgs.vscode-extensions.eww-yuck.yuck # eww (widgets)
+				#pkgs.vscode-extensions.eww-yuck.yuck # eww (not supportet?)
 			];
 		};
 	};
