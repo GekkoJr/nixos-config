30405d5f11045b5ea0e6f348a30e147b71e46e2d
GekkoJr <gekkojr@proton.me>
2025-11-12 23:20:57 +0100

    Added nerdtree to nvim

--- modules/core-programs/nvim.nix
+++ modules/core-programs/nvim.nix
@@ -9,6 +9,11 @@
     configure = {
       customRC = ''
         " custom vimscript
+        let mapleader = "\<space>"
+        let maplocalleader = "\<space>"
+
+        " nerdtree keybinds
+        nnoremap <C-e> :NERDTreeToggle<CR>
       '';
       customLuaRC = ''
         -- custom lua config
@@ -29,8 +34,19 @@
                       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                       ['<C-f>'] = cmp.mapping.scroll_docs(4),
                       ['<C-Space>'] = cmp.mapping.complete(),
-                      ['<C-e>'] = cmp.mapping.abort(),
+                      ['<C-a>'] = cmp.mapping.abort(),
                       ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                      ['<Tab>'] = cmp.mapping(function(fallback)
+                      if cmp.visible() then
+                        local entry = cmp.get_selected_entry()
+                        if not entry then 
+                          cmp.select_next_item({ behaviour = cmp.SelectBehavior.Select })
+                        end
+                        cmp.confirm()
+                      else 
+                        fallback()
+                      end
+                      end, {"i", "s",}), 
             }),
 
             sources = cmp.config.sources({
@@ -46,14 +62,34 @@
         -- setting up lspconfig
         local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+        -- lualine status bar
+        require('lualine').setup({
+          options = {
+            icons_enabled = true,
+            theme = "tomorrow_night",
+
+          },
+          sections = {
+            lualine_a = {'mode'},
+            lualine_b = {'branch', 'diagnostics'},
+            lualine_c = {'filename'},
+            lualine_x = {'lsp_status'}, 
+            lualine_y = {'filetype'},
+            lualine_z = {'location'},
+              
+          },
+        })
+
       '';
       packages.myVimPackage = with pkgs.vimPlugins; {
         # loaded on launch
         start = [
+          # utilitys
           fugitive
           vim-nix
           nvim-treesitter.withAllGrammars
           ctrlp
+          nerdtree
 
           #snippets
           cmp-vsnip
@@ -65,6 +101,11 @@
           cmp-nvim-lsp
           cmp-buffer
           cmp-emoji
+
+          # lsp
+
+          # status bar
+          lualine-nvim
         ];
         # manually loadable by calling `:packadd $plugin-name`
         opt = [ ];
