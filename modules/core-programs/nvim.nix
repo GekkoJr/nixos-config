6044285f04af0d4d7c5d75f241da8f92cbc210d5
GekkoJr <gekkojr@proton.me>
2025-11-12 01:23:25 +0100

    added some very basic autocomplete functionality

--- modules/core-programs/nvim.nix
+++ modules/core-programs/nvim.nix
@@ -13,6 +13,39 @@
       customLuaRC = ''
         -- custom lua config
         vim.cmd.colorscheme "habamax"
+
+        -- setup up nvim cpm (auto complete)
+        local cmp = require"cmp"
+
+        cmp.setup({
+            snippet = {
+                -- THIS IS REQUIRED DO NOT REMOVE 
+                expand = function(args)
+                    vim.fn["vsnip#anonymous"](args.body) -- using vsnip
+                end,
+            },
+
+            mapping = cmp.mapping.preset.insert({
+                      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                      ['<C-Space>'] = cmp.mapping.complete(),
+                      ['<C-e>'] = cmp.mapping.abort(),
+                      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            }),
+
+            sources = cmp.config.sources({
+                { name = "nvim-lsp" },
+                { name =  "vsnip"},
+                { name = "emoji" }
+
+            }, {
+                { name = "buffer" }
+            })
+        })
+
+        -- setting up lspconfig
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
       '';
       packages.myVimPackage = with pkgs.vimPlugins; {
         # loaded on launch
@@ -20,7 +53,18 @@
           fugitive
           vim-nix
           nvim-treesitter.withAllGrammars
+          ctrlp
 
+          #snippets
+          cmp-vsnip
+          vim-vsnip
+
+          # auto complete
+          nvim-cmp
+          nvim-lspconfig
+          cmp-nvim-lsp
+          cmp-buffer
+          cmp-emoji
         ];
         # manually loadable by calling `:packadd $plugin-name`
         opt = [ ];
