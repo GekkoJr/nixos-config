fc7b8e2eead53c13047367b6db910c3861248117
GekkoJr <gekkojr@proton.me>
2026-08-02 14:27:24 +0200

    Added highres audio support

--- modules/core/audio.nix
+++ modules/core/audio.nix
@@ -7,6 +7,24 @@
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
+      extraConfig = {
+        pipewire = {
+          "10-clock-rate" = {
+            "context.properties" = {
+              "default.clock.allowed_rates" = [
+                44100
+                24000
+                48000
+                256000
+                96000
+                192000
+              ];
+              "default.clock.rate" = 256000;
+            };
+          };
+
+        };
+      };
     };
   };
 }
